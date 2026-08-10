0941f8f2e9ed77c156b2b570c4f09d4552aa99d0
James Hayes (né McMahon) <james.hayes2@phs.scot>
2026-08-10 15:39:26 +0100

    Refactor comments and improve code formatting
    Updated comments for clarity and corrected formatting in the R script for postcode shapefiles processing.

--- Geospatial/Set Union Postcode Shapefiles Example.R
+++ Geospatial/Set Union Postcode Shapefiles Example.R
@@ -39,9 +39,9 @@ postcode_sf <- list.files(
   read_sf() %>%
   clean_names()
 
-# In these "postcode" shapefiles some postcodes have actually been split into parts
-# Some are in the form of a nromal postcode plues a letter e.g. o.g. postcode is G744JF but its been split into G744JFA + G744JFB
-# These parts have to be combined in order to get the actual shapes of the postcodes
+# In these "postcode" shapefiles, some postcodes have actually been split into parts
+# Some are in the form of a normal postcode plus a letter, e.g. postcode is G744JF, but it's been split into G744JFA + G744JFB
+# These parts have to be combined to get the actual shapes of the postcodes
 # + to be able to attach on the SPD
 
 postcode_sf <- postcode_sf %>%
@@ -64,16 +64,16 @@ postcode_sf <- postcode_sf %>%
 split_postcodes_sf <- postcode_sf %>%
   filter(postcode != true_postcode) %>%
   summarise(
-    # This function takes the Postcodes and combines them all together by HSCP
+    # This function takes the Postcodes and combines them all by HSCP
     geometry = st_union(geometry),
-    .by = c("true_postcode")
+    .by = "true_postcode"
   ) %>%
   rename(postcode = true_postcode)
 
 
 non_split_postcodes_sf <- postcode_sf %>%
   filter(postcode == true_postcode) %>%
-  dplyr::select(postcode)
+  select(postcode)
 
 
 postcode_sf <- non_split_postcodes_sf %>%
@@ -85,10 +85,10 @@ postcode_sf <- postcode_sf %>%
   mutate(postcode = format_postcode(postcode, format = "pc8")) %>%
   left_join(
     spd_lookup,
-    by = c("postcode" = "pc8"),
+    by = join_by(postcode == pc8),
     relationship = "one-to-one"
   ) %>%
-  dplyr::select(
+  select(
     hb2019,
     hb2019name,
     hscp2019,
@@ -103,7 +103,7 @@ postcode_sf <- postcode_sf %>%
 hscp_sf <- postcode_sf %>%
   filter(hb2019name == healthboard_oi) %>%
   summarise(
-    # This function takes the Postcodes and combines them all together by HSCP
+    # This function takes the Postcodes and combines them all by HSCP
     geometry = st_union(geometry),
     .by = c("hb2019", "hb2019name", "hscp2019", "hscp2019name")
   ) %>%
